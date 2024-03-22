a9b366bd5fce47799a2089beccff11d64490d1b0
chaechae66 <valueworkerchae@gmail.com>
2024-03-22 15:26:33 +0900

    Feat: uuid와 localstorage로 유저를 식별하고 이미지 업로드 기능 구현

--- src/components/AddDrawing/AddDrawing.tsx
+++ src/components/AddDrawing/AddDrawing.tsx
@@ -1,9 +1,28 @@
 import axios from "axios";
-import { ChangeEventHandler, FormEventHandler, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import {
+  ChangeEventHandler,
+  FormEventHandler,
+  useEffect,
+  useState,
+} from "react";
+import { LocalStorage } from "../../lib/localStorage";
 
 function AddDrawing() {
   const [imgFile, setImgFile] = useState<File | null>(null);
   const [prevImg, setPrevImg] = useState<string | null>(null);
+
+  const localStorage = new LocalStorage();
+
+  useEffect(() => {
+    if (localStorage.get()) {
+      return;
+    }
+
+    localStorage.set(uuidv4());
+  }, []);
+
   const onLoadFile: ChangeEventHandler<HTMLInputElement> = (e) => {
     const file = e.target.files![0];
     if (!file) {
@@ -18,20 +37,22 @@ function AddDrawing() {
   };
   const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
+    if (!imgFile) {
+      return;
+    }
     const formData = new FormData();
     formData.append("drawingImage", imgFile!);
+    formData.append("user", JSON.stringify(localStorage.get()));
 
-    axios
-      .post("http://localhost:4000/api/user-profile", formData, {})
-      .then((res) => {
-        console.log(res);
-      });
+    axios.post("http://localhost:4000/article", formData).then((res) => {
+      console.log(res);
+    });
   };
   return (
     <div className="w-60">
       <h3 className="text-2xl">그림</h3>
       <form onSubmit={onSubmit}>
-        <input type="file" accept="image/*" onChange={onLoadFile} />
+        <input type="file" accept=".jpg,.jpeg,.png" onChange={onLoadFile} />
         <div>
           <h4 className="text-xl">미리보기</h4>
           {prevImg ? <img src={prevImg} /> : <p>미리보기 이미지 없음</p>}
