4030a84dc0fd300b4dfcbca38d15b306fcdde344
chaechae66 <valueworkerchae@gmail.com>
2024-03-24 17:29:53 +0900

    Refactor: 리스트 페이지에서도 좋아요 기능되게 수정

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,7 +1,49 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { LocalStorage } from "../lib/localStorage";
 import { TList } from "../types/List";
 import { Link } from "react-router-dom";
+import axios, { AxiosResponse } from "axios";
 
 function ListItem({ elem }: { elem: TList }) {
+  const localStorage = new LocalStorage();
+  const userId = localStorage.get();
+  const queryClient = useQueryClient();
+
+  const { mutate: likeMutate, error: likeMutateError } = useMutation({
+    mutationKey: ["article", "like", userId, elem._id],
+    mutationFn: (): Promise<AxiosResponse> =>
+      axios.post(`http://localhost:4000/article/${elem._id}/like/${userId}`),
+    onSuccess() {
+      queryClient.invalidateQueries({
+        queryKey: ["article", "like", userId, elem._id],
+      });
+      queryClient.invalidateQueries({ queryKey: ["article"] });
+    },
+  });
+  const { data: isLike, error: likeError } = useQuery({
+    queryKey: ["article", "like", userId, elem._id],
+    queryFn: async (): Promise<TList> => {
+      return axios
+        .get(`http://localhost:4000/article/${elem._id}/like/${userId}`)
+        .then(({ data: { data } }) => {
+          if (data) {
+            return data.isLike;
+          } else {
+            return false;
+          }
+        });
+    },
+  });
+
+  const onLikeClick = () => {
+    if (!userId) {
+      alert("좋아요가 정상적으로 작동되지 않았습니다");
+    }
+    likeMutate();
+  };
+  if (likeMutateError || likeError) {
+    alert("좋아요가 정상적으로 작동되지 않았습니다");
+  }
   return (
     <div key={elem._id}>
       <Link to={`detail/${elem._id}`}>
@@ -10,6 +52,11 @@ function ListItem({ elem }: { elem: TList }) {
           alt="그림 이미지"
         />
       </Link>
+      <div className="text-xl">
+        <span onClick={onLikeClick}>{isLike ? "♥" : "♡"}</span> :
+        {elem.likeCount}
+        <span>✉️</span> :{elem.commentCount}
+      </div>
     </div>
   );
 }
