29bfe22e2981dcc7a26999d7b13046572c952a78
chaechae66 <valueworkerchae@gmail.com>
2024-03-25 16:21:28 +0900

    Chore: 라우터 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Detail from "../pages/Detail";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ const router = createBrowserRouter([
   {
     path: "/detail/:id",
     element: <Detail />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
   },
 ]);
 
